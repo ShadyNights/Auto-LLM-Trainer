1a49c2f7590993f149b9fa4a0bc6aedd64a4a0cd
ShadyNights <shadynights87@gmail.com>
2026-07-09 12:29:22 +0530

    Resolve components namespace collision

--- delete_components_script.py
+++
@@ -1,6 +0,0 @@
-import os
-
-file_to_delete = r"d:\Traveler LLM\src\ui\components.py"
-if os.path.exists(file_to_delete):
-    os.remove(file_to_delete)
-    print(f"Deleted {file_to_delete}")
--- delete_pages_script.py
+++
@@ -1,7 +0,0 @@
-import shutil
-import os
-
-pages_dir = r"d:\Traveler LLM\src\ui\pages"
-if os.path.exists(pages_dir):
-    shutil.rmtree(pages_dir)
-    print(f"Deleted {pages_dir}")
